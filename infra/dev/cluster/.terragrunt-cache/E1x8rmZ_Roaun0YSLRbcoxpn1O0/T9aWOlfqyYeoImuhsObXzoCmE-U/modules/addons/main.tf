1764a845681ec16f3d7bd01599f7cd3129484fb2
Toni744 <femi.obatoki@gmail.com>
2026-03-08 06:51:38 +0000

    update attribute

--- infra/dev/cluster/.terragrunt-cache/E1x8rmZ_Roaun0YSLRbcoxpn1O0/T9aWOlfqyYeoImuhsObXzoCmE-U/modules/addons/main.tf
+++ infra/dev/cluster/.terragrunt-cache/E1x8rmZ_Roaun0YSLRbcoxpn1O0/T9aWOlfqyYeoImuhsObXzoCmE-U/modules/addons/main.tf
@@ -20,7 +20,6 @@ resource "aws_eks_addon" "vpc_cni" {
   cluster_name             = var.cluster_name
   addon_name               = "vpc-cni"
   addon_version            = data.aws_eks_addon_version.vpc_cni.version
-  resolve_conflicts        = "OVERWRITE"
   service_account_role_arn = var.vpc_cni_role_arn
 
   tags = var.tags
@@ -38,7 +37,6 @@ resource "aws_eks_addon" "kube_proxy" {
   cluster_name      = var.cluster_name
   addon_name        = "kube-proxy"
   addon_version     = data.aws_eks_addon_version.kube_proxy.version
-  resolve_conflicts = "OVERWRITE"
 
   tags = var.tags
 }
